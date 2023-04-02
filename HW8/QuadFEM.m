b854cb1c7f3b8e587b270b9a6d60005fd9ab83be
Andi-Zhou-1324 <andizhou@umich.edu>
2023-04-01 23:28:36 -0400

    Commit

--- HW8/QuadFEM.m
+++ HW8/QuadFEM.m
@@ -1,7 +1,7 @@
 clear
 close all
 
-N_vec = [1,25,100,400,1600,6400,8100];
+N_vec = [1];
 tau_vec = zeros(3,size(N_vec,2));
 for count = 1:size(N_vec,2)
     N = N_vec(count);
@@ -86,6 +86,9 @@ for count = 1:size(N_vec,2)
     
     node_elem = e(elem,:);
     
+    node_elem_temp = node_elem(3);
+    node_elem(3) = node_elem(4);
+    node_elem(4) = node_elem_temp;
     xi = abs(1-co(node_elem(1),1))./(abs(co(node_elem(1)) - co(node_elem(2),1)));
     eta = 0;
     
